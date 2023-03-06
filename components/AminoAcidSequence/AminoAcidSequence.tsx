0fb9c5714901d1143b8d9379a9fb944fe03b79e9
Elliot Hershberg <eahershberg@gmail.com>
2023-03-06 12:19:49 -0800

    Improve aa styling

--- components/AminoAcidSequence/AminoAcidSequence.tsx
+++ components/AminoAcidSequence/AminoAcidSequence.tsx
@@ -15,8 +15,14 @@ function AminoAcidSequence({ sequence }: AminoAcidSequenceProps) {
     <div className="flex flex-wrap w-2/3">
       {aminoAcidArray.map((aminoAcid) => {
         return (
-          <div className="border border-black w-6 h-6 flex items-center justify-center m-2 p-6 rounded">
-            {aminoAcid.position}
+          <div className="border border-gray-900 w-6 h-6 flex items-center justify-center m-2 p-6 rounded">
+            <div>
+              <strong className="text-xl text-gray-900 font-bold">
+                {aminoAcid.initialAminoAcid}
+              </strong>
+              <br />
+              <p className="text-sm text-gray-600">{aminoAcid.position}</p>
+            </div>
           </div>
         );
       })}
