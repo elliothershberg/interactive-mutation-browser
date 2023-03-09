3cd7e4d7ec1948b4585e1e38c0af08ceff053f7a
Elliot Hershberg <eahershberg@gmail.com>
2023-03-09 15:08:01 -0800

    Check in temp solution

--- components/StructureViewer/StructureView.tsx
+++ components/StructureViewer/StructureView.tsx
@@ -4,9 +4,8 @@ import { useEffect, useState } from "react";
 function StructureViewer({ sequence }: { sequence: string }) {
   const [structure, setStructure] = useState<any>(null);
   const [loading, setLoading] = useState(false);
-  // state for keeping track of the 3Dmol.js scripts loading
+  // state for keeping track of the 3Dmol.js script loading
   const [viewerLoaded, setviewerLoaded] = useState(false);
-  const [uiLoaded, setUiLoaded] = useState(false);
 
   const fetchStructure = async () => {
     const response = await fetch("api/structure", {
@@ -33,40 +32,40 @@ function StructureViewer({ sequence }: { sequence: string }) {
     });
   }, [sequence]);
 
-  // need to wait for the 3Dmol.js scripts to load,
+  // need to wait for the structure to be fetched,
   // then initialize the viewer
   useEffect(() => {
-    if (viewerLoaded && uiLoaded) {
-      // @ts-ignore
-      globalThis.$3Dmol.autoload();
+    const { $3Dmol } = globalThis as any;
+    if (viewerLoaded && $3Dmol) {
+      $3Dmol.autoload();
     }
-  }, [viewerLoaded, uiLoaded]);
+  }, [structure]);
 
   return (
     <>
       <Script
         src="https://3Dmol.org/build/3Dmol-min.js"
         onLoad={() => {
-          console.log("loaded!");
           setviewerLoaded(true);
         }}
       ></Script>
-      <Script
-        src="https://3Dmol.org/build/3Dmol.ui-min.js"
-        onLoad={() => {
-          setUiLoaded(true);
-        }}
-      ></Script>
       <div className="m-16">
-        {viewerLoaded && uiLoaded && (
+        {structure && (
           <div className="w-full h-full">
-            Container:
+            <textarea
+              className="hidden"
+              id="structure-data"
+              value={structure.message}
+              readOnly
+            ></textarea>
             <div
               style={{ height: 400, width: 400, position: "relative" }}
               className="viewer_3Dmoljs"
-              data-pdb="2POR"
+              data-element="structure-data"
+              data-type="pdb"
               data-backgroundcolor="0xffffff"
-              data-style="stick"
+              data-style="cartoon:color=spectrum"
+              // data-style="stick"
               data-ui="true"
             ></div>
           </div>
