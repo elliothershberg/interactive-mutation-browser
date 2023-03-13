0603eed5d5b9494ddf9d6927f3ad884f4a34041e
Elliot Hershberg <eahershberg@gmail.com>
2023-03-13 14:27:24 -0700

    Add devtools within provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,12 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 
 import { Provider } from "jotai";
+import { DevTools } from "jotai-devtools";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Provider>
+      <DevTools />
       <Component {...pageProps} />
     </Provider>
   );
