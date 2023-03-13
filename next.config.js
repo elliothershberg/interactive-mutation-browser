1c1cdf1926b87e695c03b555b01d6a727158104a
Elliot Hershberg <eahershberg@gmail.com>
2023-03-13 14:26:48 -0700

    Add jotai devtools dep

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  // Learn more here - https://nextjs.org/docs/advanced-features/compiler#module-transpilation
+  // Required for UI css to be transpiled correctly 👇
+  transpilePackages: ["jotai-devtools"],
 };
