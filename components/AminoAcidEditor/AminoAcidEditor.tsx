86a09539d2e1704d359d40ce3a300b3c15499eff
Elliot Hershberg <eahershberg@gmail.com>
2023-03-27 10:42:02 -0700

    Add explanation

--- components/AminoAcidEditor/AminoAcidEditor.tsx
+++ components/AminoAcidEditor/AminoAcidEditor.tsx
@@ -3,9 +3,16 @@ import React from "react";
 import AminoAcidTrack from "./AminoAcidTrack";
 import AminoAcidSequence from "./AminoAcidSequence";
 
+import InfoPopover from "./InfoPopover";
+
 function AminoAcidEditor() {
   return (
     <div className="w-2/3">
+      <div className="flex justify-start ml-4 mb-6 text-xl text-gray-700">
+        Current mutations
+        <InfoPopover />
+      </div>
+
       <AminoAcidTrack />
       <AminoAcidSequence />
     </div>
---
+++ components/AminoAcidEditor/InfoPopover.tsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import { Popover } from "@headlessui/react";
+import { usePopper } from "react-popper";
+import { QuestionMarkCircleIcon } from "@heroicons/react/24/outline";
+
+function InfoPopover() {
+  let [referenceElement, setReferenceElement] = useState<HTMLElement | null>(
+    null
+  );
+  let [popperElement, setPopperElement] = useState<HTMLElement | null>();
+  let { styles, attributes } = usePopper(referenceElement, popperElement, {
+    placement: "right",
+  });
+
+  return (
+    <Popover>
+      <Popover.Button ref={setReferenceElement} className="focus:outline-none">
+        <QuestionMarkCircleIcon
+          className="h-6 w-6 pl-1 pt-1"
+          aria-hidden="true"
+        />
+      </Popover.Button>
+
+      <Popover.Panel
+        ref={setPopperElement}
+        style={styles.popper}
+        {...attributes.popper}
+      >
+        <div className="bg-white rounded-lg shadow-lg p-4 border border-gray-900 flex flex-wrap mt-2 text-gray-600 text-sm text-left ml-3">
+          Use the interactive brush to navigate around the sequence.
+          <br />
+          Click on an amino acid to make a mutation.
+        </div>
+      </Popover.Panel>
+    </Popover>
+  );
+}
+
+export default InfoPopover;
