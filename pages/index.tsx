fed81c081fe4197e54101394225f2665b716e4d2
Elliot Hershberg <eahershberg@gmail.com>
2023-04-15 13:41:55 -0700

    Fix home page title

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,7 @@ const Home: NextPage = () => {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
       <Head>
-        <title>Create Next App</title>
+        <title>Interactive Mutation Browser</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
